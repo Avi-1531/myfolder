06b3cdf209f08235a1bdc644f88e6fa8a3f5a20d
Avi Chauhan <117462878+Avi-1531@users.noreply.github.com>
2025-09-08 10:37:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,23 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+data resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
+data resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/22"]
+}
+
+# Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
