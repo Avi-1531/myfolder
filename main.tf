af7607676345d8e327b99a3ad06d86e7b707429a
Avi Chauhan <117462878+Avi-1531@users.noreply.github.com>
2025-09-08 10:55:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -8,22 +6,19 @@ terraform {
     }
   }
 }
-#comment
 
 provider "azurerm" {
   features {}
-
-
 }
 
-# Create a resource group
-data  "azurerm_resource_group" "example" {
+# Resource group
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-# Create a virtual network within the resource group
-data  "azurerm_virtual_network" "example" {
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -37,3 +32,53 @@ resource "azurerm_subnet" "example" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# Public IP (for RDP access)
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Windows VM
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-win-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2ms"
+  admin_username      = "avi"
+  admin_password      = "map@15311531"   # ⚠️ Change this or use Key Vault/secrets
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    name                 = "example-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
